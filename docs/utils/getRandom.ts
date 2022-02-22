f94b8aacfa9cdc6079afd0c5d0e1a7b1cd677ff1
davidsolis41 <davidsolisalarco@gmail.com>
2022-02-22 15:43:25 -0600

    v1

--- docs/utils/getRandom.ts
+++ docs/utils/getRandom.ts
@@ -1,5 +1,6 @@
 export default function getRandom(): number {
-  let primerNumero = Math.random() * (100000000000 - 1) + 1;
-  let segundoNumero = Math.random() * (100000000000 - 1) + 1;
-  return Number(primerNumero * segundoNumero);
+  return Number(
+    (Math.random() * (100000000000 - 1) + 1) *
+      (Math.random() * (100000000000 - 1) + 1)
+  );
 }
