49f5ae1f75e947b0492e27f9be32be3df0dd0de6
davidsolis41 <davidsolisalarco@gmail.com>
2022-02-23 16:59:22 -0600

    v1

--- docs/components/@muii/informativos/tooltip.tsx
+++ docs/components/@muii/informativos/tooltip.tsx
@@ -1,11 +1,42 @@
 import React from "react";
-import Tooltip from "@mui/material/Tooltip";
+import Tooltip, { TooltipProps, tooltipClasses } from "@mui/material/Tooltip";
 import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import Fade from "@mui/material/Fade";
 
 export default function Tooltipp() {
   return (
-    <Tooltip title="">
-      <Button></Button>
+    <Tooltip
+      title="Enviar el Formulario" // TAMBIEN PUEDEN SER COMPONENTES HTML
+      placement="top" // top-start | top | top-end | left-start | left | left-end | ETC....
+      enterDelay={500} // OPCIONAL tiempo de entrada
+      leaveDelay={200} // OPCIONAL tiempo de salida
+      TransitionComponent={Fade} // OPCIONAL para cambiar la trancision
+      TransitionProps={{ timeout: 600 }} // OPCIONAL para cambiar las props de trancision
+      arrow // con flecha
+      disableInteractive // OPCIONAL no se puede posicionar sobre el tootip
+      followCursor // OPCIONAL para que siga al cursor mientras se mueve
+    >
+      <Button>Enviar</Button>
     </Tooltip>
   );
 }
+
+const CustomTooltip = styled(({ className, ...props }: TooltipProps) => (
+  <Tooltip {...props} arrow classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.arrow}`]: {
+    color: theme.palette.common.black,
+  },
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: theme.palette.common.black,
+  },
+}));
+
+const CustomWidthTooltip = styled(({ className, ...props }: TooltipProps) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))({
+  [`& .${tooltipClasses.tooltip}`]: {
+    maxWidth: 500,
+  },
+});
