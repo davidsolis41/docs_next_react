ee90f36abddb7192199ed59dfe538327bdda438e
davidsolis41 <davidsolisalarco@gmail.com>
2022-03-01 16:00:53 -0600

    v1

---
+++ docs/components/@muii/cargando/skeleton.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Skeleton from "@mui/material/Skeleton";
+
+export default function Skeletonn() {
+  return (
+    <Skeleton
+      variant="rectangular" // rectangular | circular | text
+      animation="pulse" // pulse | wave | false
+      width={210}
+      height={118}
+      sx={{ bgcolor: "grey.900" }} // OPCIONAL cambiar el color
+    />
+  );
+}
+
+/* ------------------------------- Inferir Tamaño -------------------------------*/
+// Podemos pasar el skeleton como hijo condicional
+
+// <Typography variant="h1">{loading ? <Skeleton /> : 'h1'}</Typography>
+
+// O Podemos asignarle un hijo a skeleton
+
+// <Skeleton variant="circular"> <Avatar /> </Skeleton>
