1708ae065be9b39309aaa24233f453e83cf9adb0
davidsolis41 <davidsolisalarco@gmail.com>
2022-03-02 14:10:02 -0600

    v1

--- docs/components/@muii/contenedores/box.tsx
+++ docs/components/@muii/contenedores/box.tsx
@@ -1,18 +1,6 @@
 import Box from "@mui/material/Box";
 import React from "react";
 
-function box() {
-  return (
-    <Box
-      component="section"
-      onClick={() => {}}
-      onDoubleClick={() => {}}
-      className=""
-      style={{}}
-      sx={{}}
-      id=""
-    ></Box>
-  );
+export default function box() {
+  return <Box component="section" id="" className="" style={{}} sx={{}}></Box>;
 }
-
-export default Box;
