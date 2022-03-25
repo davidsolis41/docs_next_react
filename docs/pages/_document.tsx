385438ebda45e63260ef327c41c1ca53166ee777
davidsolis41 <73626662+davidsolis41@users.noreply.github.com>
2022-03-25 11:11:20 -0600

    v1

--- docs/pages/_document.tsx
+++ docs/pages/_document.tsx
@@ -1,9 +1,18 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from "next/document";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: any) {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
     const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    return initialProps;
   }
 
   render() {
